0dfdc8e29542e20cf352a7b63464b8d8d161d9d4
Joseph Grainger <grrainger7@gmail.com>
2021-03-29 14:00:40 +0200

    finished

---
+++ hello.js
@@ -0,0 +1,16 @@
+console.log("testing");
+
+const value="this is the value";
+console.log(value);
+
+const firstName="Joseph";
+const lastName="Grainger";
+console.log(firstName, lastName);
+
+const personName="Sheridan Jones";
+const city="Sheffield";
+const job="Fisherwoman";
+const age="38";
+const hobby="candlemaking";
+console.log(personName + " is a " + job + " living in " + city + ". " + "She is " + age + " years old and lists " + hobby + " as a hobby.");
+
